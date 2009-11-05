f6dc6e86449e92bce803b4cf264082e09aba7f89
goschtl <goschtl@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2009-11-05 13:47:11 +0000

    search for Artikel

--- mediashop/trunk/src/nva.mediashop/nva/mediashop/browser/mediashop.py
+++ mediashop/trunk/src/nva.mediashop/nva/mediashop/browser/mediashop.py
@@ -1,18 +1,29 @@
 from five import grok
 from nva.mediashop.interfaces import IMedienShop, IKategorie
+from Products.CMFCore.utils import getToolByName
+from Products.statusmessages.interfaces import IStatusMessage
 
 grok.templatedir('templates')
 
 class Index(grok.Form):
     grok.context(IMedienShop)
     categories = []
+    results = False
+
+    @property
+    def portal_catalog(self):
+        return getToolByName(self.context, 'portal_catalog')
 
     def update(self):
-        request = self.request
-        print request.text()
         self.categories = self.context.objectIds()
-
 
     @grok.action('Suchen')
     def handle_search(self, **kw):
-        import pdb; pdb.set_trace() 
+        artikel = self.request.get('form.artikel', None)
+        if artikel:
+            self.results = self.portal_catalog(portal_type="Artikel", searchableText=artikel)
+            message = "Es wurden %s Ergebnisse gefunden" %len(self.results)
+        else:
+            message="Bitte geben Sie einen Suchbegriff ein" 
+         
+        IStatusMessage(self.request).addStatusMessage(message, type="info")
