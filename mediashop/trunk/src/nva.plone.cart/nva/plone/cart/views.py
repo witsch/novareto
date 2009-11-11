aa4efd9fa529149b1f86f466744c1f979eae7145
goschtl <goschtl@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2009-11-11 11:15:23 +0000

    Now the checkoutform works

--- mediashop/trunk/src/nva.plone.cart/nva/plone/cart/views.py
+++ mediashop/trunk/src/nva.plone.cart/nva/plone/cart/views.py
@@ -10,7 +10,24 @@ from zope.publisher.interfaces import NotFound
 from zope.publisher.interfaces.http import IHTTPRequest
 from nva.cart import ICartAddable, ICartRetriever, ICartHandler
 
-from plone.app.layout.viewlets.interfaces import IAboveContent
+from plone.app.layout.viewlets.interfaces import IBelowContentBody
+from nva.mediashop.interfaces import IOrderForm
+from zope.formlib import form
+from Products.statusmessages.interfaces import IStatusMessage
+from Products.CMFCore.utils import getToolByName
+
+
+def null_validator(*args, **kwargs):
+    """A validator that doesn't validate anything.
+    
+    This is somewhat lame, but if you have a "Cancel" type button that
+    won't want to validate the form, you need something like this.
+
+    @form.action(_(u"label_cancel", default=u"Cancel"),
+                 validator=null_validator,
+                 name=u'cancel')
+    """
+    return ()
 
 
 class IPloneCart(Interface):
@@ -20,7 +37,7 @@ class IPloneCart(Interface):
 
 
 class AddToCartLink(grok.Viewlet):
-    grok.viewletmanager(IAboveContent)
+    grok.viewletmanager(IBelowContentBody)
     grok.context(ICartAddable)
 
 
@@ -39,13 +56,63 @@ class AddToCart(grok.View):
         self.redirect(self.url(self.context))
 
 
-class Checkout(grok.View):
+class Checkout(grok.Form):
     """A view for the Plone cart
     """
     grok.context(IPloneCart)
+    lable="Bestellformular"
+    form_name="Bitte geben Sie alle Werte ein."
+    form_fields = grok.Fields(IOrderForm)
 
-    def render(self):
-        return u"Here should be the checkout form."
+    @property
+    def portal(self):
+        return getToolByName(self.context, 'portal_url').getPortalObject()
+
+    @form.action(u'Abbrechen', validator=null_validator)
+    def handle_cancel(self, action, data):
+        self.context.cart.clear()
+        view_url = self.portal.absolute_url()
+        IStatusMessage(self.request).addStatusMessage("Der Bestellvorgang wurde abgebrochen.", type='info')
+        self.request.response.redirect(view_url)
+
+
+    @form.action(u'Bestellen')
+    def handle_order(self, action, data):
+        print data, action
+
+    def renderField(self, *args):
+        label = required = description = error = input = "" 
+        css_class = "field "
+        for field in args:
+            label += "%s " %field.label 
+            if field.required:
+                required ='<span class="fieldRequired" title="Required" tal:condition="plz/required"> (Required) </span>'
+            if field.hint:
+                description += "%s " % field.hint
+            if field.error():
+                error += "%s " %field.error()
+            input += "%s " %field()    
+            id = field.name.replace('.', '_')
+            css_class += "%s_" %field.name
+        if len(error.strip()) > 0:
+            css_class = css_class + ' error'
+        ret = """
+        <div id="%s" class="%s">
+           <label> %s </label>
+           %s
+           <div class="formHelp">%s</div>
+           <div class="fieldErrorBox"> %s </div>
+           <span class="widget"> %s </span>
+        </div>""" %(id, css_class, label, required, description, error, input)
+        return ret 
+
+    def default_namespace(self):
+        namespace = grok.View.default_namespace(self)
+        namespace['cart'] = self.context.cart
+        namespace['handler'] = ICartHandler(self.context.cart)
+        namespace['context'] = self.context.parent
+        namespace['here'] = self.context.parent
+        return namespace
 
 
 class CartView(grok.View):
