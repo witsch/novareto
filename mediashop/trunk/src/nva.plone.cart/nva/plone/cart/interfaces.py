8bc378a0dcf3dd9e82041e30ae5960f2f19bccd7
trollfot <trollfot@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2009-11-13 10:13:30 +0000

    Added date.

--- mediashop/trunk/src/nva.plone.cart/nva/plone/cart/interfaces.py
+++ mediashop/trunk/src/nva.plone.cart/nva/plone/cart/interfaces.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from zope.schema import Object, TextLine
+from zope.schema import Object, TextLine, Float, Bool
 from zope.interface import Interface, Attribute
 from zope.app.container.constraints import contains
 from nva.cart import ICartHandler
@@ -8,7 +8,12 @@ from nva.cart import ICartHandler
 
 class ICartWrapper(Interface):
     """An object that wraps a cart.
-    """    
+    """
+    is_member = Bool(
+        title = u"The cart owner is a member",
+        default = False
+        )
+
     def __init__(cart, *arg, **kw):
         """Initialize the cart wrapper.
         """
@@ -16,7 +21,7 @@ class ICartWrapper(Interface):
 
 class ISessionCart(ICartWrapper):
     """A cart for Plone
-    """
+    """        
     handler = Object(
         title = u"Cart controler allowing CRUD operations",
         schema = ICartHandler,
@@ -30,6 +35,10 @@ class IOrder(ICartWrapper):
     reference = TextLine(
         title = u"Reference of the order",
         readonly = True
+        )
+
+    total_price = Float(
+        title = u"Pinned price of the order.",
         )
 
 
