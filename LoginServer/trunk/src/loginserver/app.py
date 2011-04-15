c7b7752960b60251ae3cf89ae5ad191ec9bb24fd
goschtl <goschtl@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2011-04-15 07:47:28 +0000

    changes

--- LoginServer/trunk/src/loginserver/app.py
+++ LoginServer/trunk/src/loginserver/app.py
@@ -1,21 +1,21 @@
-import grok
 
-from loginserver import resource
-from zope.interface import Interface
-from zope.schema import TextLine, Password
-from M2Crypto import RSA
 import auth_pubtkt
-import time, datetime
-import os.path
-import loginserver
-from urllib import quote
+import datetime
 import base64
+import grok
+import time
 import xmlrpclib
+import zope.app.appsetup.product
+
+
+from M2Crypto import RSA
 from grokcore.message import receive
+from urllib import quote
 
 
-privkey_path = os.path.join(os.path.dirname(loginserver.__file__), 'privkey.pem')
-privkey = RSA.load_key(privkey_path)
+config = zope.app.appsetup.product.getProductConfiguration('loginserver')
+
+privkey = RSA.load_key(config.get('private_key_path'))
 
 
 class Loginserver(grok.Application, grok.Container):
@@ -30,28 +30,32 @@ class Index(grok.View):
         if not self.checkRule(login, password):
             return
         if self.checkAuth(login, password) == 0:
-            self.flash(u'Benutzername und oder Passwort nicht richtig')
+            self.flash(config.get('wrong_credentials'))
             return
+
         request = self.request
         val = base64.encodestring('%s:%s' % (login, password))
-        validuntil = int(time.mktime(datetime.datetime(2012, 4, 1).timetuple()))
-        ticket = auth_pubtkt.create_ticket(privkey, '0000-00025', validuntil, tokens=['su'])
+        validuntil = int(time.mktime(
+            datetime.datetime(2012, 4, 1).timetuple()))
+        ticket = auth_pubtkt.create_ticket(
+            privkey, login, validuntil, tokens=['su'])
         request.response.setHeader(
-            'Set-Cookie:', 
-            'auth_pubtkt=%s;domain=novareto.de;path=/' % quote(ticket))
+            'Set-Cookie:',
+            'auth_pubtkt=%s;domain=%s;path=/' % (
+            quote(ticket),
+            config.get('cookie_domain')))
         request.response.setCookie(
-            'dolmen.authcookie', 
-            quote(val), 
-            path="/", 
-            domain="novareto.de")
-        # Flash Messages
-        return request.response.redirect('http://extranet.novareto.de', trusted=True)
+            'dolmen.authcookie',
+            quote(val),
+            path="/",
+            domain=config.get('cookie_domain'))
+        return request.response.redirect(config.get('backurl'), trusted=True)
 
     def checkRule(self, login, password):
         return True
 
     def checkAuth(self, login, password):
-        server = "http://192.168.2.167:8080/app"
+        server = "http://10.30.4.78:8888/app"
         portal = xmlrpclib.Server(server)
         return portal.checkAuth(login, password)
 
