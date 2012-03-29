5dfb8582ab925ff2d49da231a227b891f8e091b3
ck@novareto.de <ck@novareto.de@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2012-03-29 11:04:20 +0000

    added stammdaten

---
+++ ukh_userhandling/trunk/src/ukh_userhandling/static/stammdaten.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('input#form-field-email').attr('maxlength', '50');
+  $('input#form-field-vname').attr('maxlength', '30');
+  $('input#form-field-nname').attr('maxlength', '30');
+  $('input#form-field-vwhl').attr('maxlength', '6');
+  $('input#form-field-tlnr').attr('maxlength', '15');
+  $('input#form-field-funktion').attr('maxlength', '50');
+  $('input#form-field-titel').attr('maxlength', '15');
+});
