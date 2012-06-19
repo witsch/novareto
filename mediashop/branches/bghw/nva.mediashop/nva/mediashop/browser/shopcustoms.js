6da35f5f420263b56c66cb323b8d719103387401
lwalthernovareto@gmail.com <lwalthernovareto@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2012-06-19 10:14:08 +0000

    Javascript für individuelle Anpassungen am Bestellformular eingefügt, Anpassung an den Suchformen

---
+++ mediashop/branches/bghw/nva.mediashop/nva/mediashop/browser/shopcustoms.js
@@ -0,0 +1,5 @@
+jq(document).ready(function() {
+
+    jq('#field_form_agb').children('label').replaceWith('<label>Allgemeine Geschäftsbedingungen <a target="_blank" href="http://www.bghw.de/medienangebot/allgemeine-geschaeftsbedingungen-agb">(AGB) der BGHW</a></label>');
+
+});
