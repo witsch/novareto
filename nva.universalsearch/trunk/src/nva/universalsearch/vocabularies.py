9aaac4c5a848c09bd056a785fab8517482f8f41f
andi@pyfidelity.com <andi@pyfidelity.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2012-06-21 10:58:00 +0000

    fix vocabulary to always include previously set values for 'systems'

--- nva.universalsearch/trunk/src/nva/universalsearch/vocabularies.py
+++ nva.universalsearch/trunk/src/nva/universalsearch/vocabularies.py
@@ -1,10 +1,11 @@
-from zope.component import queryUtility
+from zope.component import queryUtility, getUtility
 from zope.interface import implements
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
 
 from collective.solr.interfaces import ISolrConnectionManager
 from collective.solr.interfaces import ISearch
+from nva.universalsearch.interfaces import IUniversalSearchConfig
 
 
 class Systems(object):
@@ -12,11 +13,12 @@ class Systems(object):
     implements(IVocabularyFactory)
 
     def __call__(self, context):
-        items = []
+        config = getUtility(IUniversalSearchConfig)
+        items = set(config.systems)
         manager = queryUtility(ISolrConnectionManager)
         if manager is not None and manager.getSchema() is not None:
             search = queryUtility(ISearch)
             params = {'rows': 0, 'facet': 'true', 'facet.field': 'system'}
             result = search('+system:*', **params)
-            items = sorted(result.facet_counts['facet_fields']['system'])
-        return SimpleVocabulary([SimpleTerm(item) for item in items])
+            items.update(result.facet_counts['facet_fields']['system'])
+        return SimpleVocabulary([SimpleTerm(item) for item in sorted(items)])
