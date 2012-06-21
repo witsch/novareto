2e05782f6788457a52ca0132fff54afed315e455
andi@pyfidelity.com <andi@pyfidelity.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2012-06-21 08:56:38 +0000

    use higher-level search utiltity to make the test more readable

--- nva.universalsearch/trunk/src/nva/universalsearch/tests/test_indexes.py
+++ nva.universalsearch/trunk/src/nva/universalsearch/tests/test_indexes.py
@@ -4,10 +4,8 @@ from nva.universalsearch.tests.base import SolrTestCase
 from transaction import abort, commit
 from zope.component import getUtility
 from collective.solr.interfaces import ISolrConnectionConfig
-from collective.solr.interfaces import ISolrConnectionManager
 from collective.solr.interfaces import ISearch
 from collective.solr.utils import activate
-from collective.solr.tests.utils import numFound
 
 
 class SolrServerTests(SolrTestCase):
@@ -30,11 +28,9 @@ class SolrServerTests(SolrTestCase):
 
     def testObjectCanBeSearchedViaSystemIndex(self):
         self.folder.processForm(values={'title': 'Foo'})
-        connection = getUtility(ISolrConnectionManager).getConnection()
         commit()                        # indexing happens on commit
         system = self.portal.Title()
-        result = connection.search(q='+system:"%s"' % system).read()
-        self.assertEqual(numFound(result), 1)
+        self.assertEqual(len(self.search('+system:"%s"' % system)), 1)
 
     def testFullUriIsStoredInSolr(self):
         self.folder.processForm(values={'title': 'Foo'})
