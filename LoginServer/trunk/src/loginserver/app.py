1dcac309a8818a6627c27768369196c661a5f795
goschtl <goschtl@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2012-07-18 07:31:16 +0000

    changes

--- LoginServer/trunk/src/loginserver/app.py
+++ LoginServer/trunk/src/loginserver/app.py
@@ -1,4 +1,4 @@
-
+import os
 import auth_pubtkt
 import datetime
 import base64
@@ -21,8 +21,32 @@ privkey = RSA.load_key(config.get('private_key_path'))
 class Loginserver(grok.Application, grok.Container):
     pass
 
+from M2Crypto import EVP
+import StringIO
+
+iv = os.urandom(16)
+
+
+
 
 class Index(grok.View):
+
+    def bauth(self, val):
+
+        def encrypt(data, key):
+            # Zero padding
+            if len(data) % 16 != 0:
+                data += '\0' * (16 - len(data) % 16);
+            buffer = StringIO.StringIO()
+            cipher = EVP.Cipher('aes_128_cbc', key=key, iv=iv, op=1)
+            cipher.set_padding(0)
+            buffer.write(cipher.update(str(data)))
+            buffer.write(cipher.final())
+            data = iv + buffer.getvalue()
+            print "DATA", data
+            return data
+        return encrypt(val, '1234567890ABCDEF')
+
 
     def update(self, login=None, password=None, anmelden=None):
         if not anmelden:
@@ -34,31 +58,35 @@ class Index(grok.View):
             return
 
         request = self.request
-        val = base64.encodestring('%s:%s' % (login, password))
+        val = base64.encodestring(self.bauth('%s:%s' % (login, password)))
         validuntil = int(time.mktime(
-            datetime.datetime(2012, 4, 1).timetuple()))
+            datetime.datetime(2012, 12, 1).timetuple()))
+        print val
         ticket = auth_pubtkt.create_ticket(
-            privkey, login, validuntil, tokens=['su'])
+                privkey, login, validuntil, tokens=['su'], extra_fields=(('bauth', val),))
         request.response.setHeader(
             'Set-Cookie',
-            'auth_pubtkt=%s;domain=%s;path=/; Secure' % (
+            'auth_pubtkt=%s;domain=%s;path=/;' % (
             quote(ticket),
             config.get('cookie_domain')))
-        request.response.setCookie(
-            'dolmen.authcookie',
-            quote(val),
-            path="/",
-            secure=True,
-            domain=config.get('cookie_domain'))
+
+        #request.response.setCookie(
+        #    'dolmen.authcookie',
+        #    quote(val),
+        #    path="/",
+        #    secure=False,
+        #    domain=config.get('cookie_domain'))
+
+        print "SETET COOKIE", ticket
         return request.response.redirect(config.get('backurl'), trusted=True)
 
     def checkRule(self, login, password):
         return True
 
     def checkAuth(self, login, password):
-        server = "http://10.30.4.78:8888/app"
-        portal = xmlrpclib.Server(server)
-        return portal.checkAuth(login, password)
+        if login == "admin" and password == "admin":
+            return 1
+        return
 
     @property
     def messages(self):
