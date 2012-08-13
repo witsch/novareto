4f8a6691f61ece6238efb973cec7f7b0b644e325
zopyxfilter@gmail.com <zopyxfilter@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2012-08-13 07:43:12 +0000

    added

---
+++ nva.borrow/trunk/nva/borrow/tests/base.py
@@ -0,0 +1,64 @@
+################################################################
+# nva.borrow
+# (C) 2012, ZOPYX Ltd.
+################################################################
+
+import os
+import unittest2
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import setRoles, login
+from plone.testing import z2
+
+from zope.configuration import xmlconfig
+from AccessControl.SecurityManagement import newSecurityManager
+from zope.component import getUtility
+
+import nva.borrow
+
+os.environ['TESTING'] = '1'
+
+class PolicyFixture(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+
+        for mod in (nva.borrow,): 
+            xmlconfig.file('configure.zcml', mod, context=configurationContext)
+        z2.installProduct(app, 'nva.borrow')
+
+    def setUpPloneSite(self, portal):
+        # Install into Plone site using portal_setup
+        applyProfile(portal, 'nva.borrow:default')
+        portal.acl_users.userFolderAddUser('god', 'dummy', ['Manager'], []) 
+        setRoles(portal, 'god', ['Manager'])
+        portal.acl_users.userFolderAddUser('ppr', 'dummy', ['PPR'], []) 
+        setRoles(portal, 'ppr', ['Member', 'PPR'])
+        portal.acl_users.userFolderAddUser('member', 'dummy', ['Member'], []) 
+        setRoles(portal, 'member', ['Member'])
+        login(portal, 'god')
+
+    def tearDownZope(self, app):
+        # Uninstall product
+        z2.uninstallProduct(app, 'nva.borrow')
+
+
+POLICY_FIXTURE = PolicyFixture()
+POLICY_INTEGRATION_TESTING = IntegrationTesting(bases=(POLICY_FIXTURE,), name="PolicyFixture:Integration")
+
+class TestBase(unittest2.TestCase):
+
+    layer = POLICY_INTEGRATION_TESTING
+
+    @property
+    def portal(self):
+        return self.layer['portal']
+
+    def login(self, uid='god'):
+        """ Login as manager """
+        user = self.portal.acl_users.getUser(uid)
+        newSecurityManager(None, user.__of__(self.portal.acl_users))
+
