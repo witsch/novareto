8ca9201fcf59efe7d54cc0219350d211bfff9c2b
zopyxfilter@gmail.com <zopyxfilter@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2012-08-14 13:15:03 +0000

    no PDB

--- nva.borrow/trunk/nva/borrow/browser/vocabulary.py
+++ nva.borrow/trunk/nva/borrow/browser/vocabulary.py
@@ -21,7 +21,6 @@ class Vocabulary(BrowserView):
     def getData(self):                   
 
         catalog = getToolByName(self.context, 'portal_catalog')
-        import pdb; pdb.set_trace() 
         dl = atapi.DisplayList()
         for brain in catalog({'portal_type' : ['nva.borrow.borrowableitem', 'nva.borrow.borrowableitems']}):
             dl.add(brain.getId, brain.Title)
