2d3169cb34beb1e56a688fbf2e01ca0b1ce98099
zopyxfilter <zopyxfilter@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2012-10-09 07:42:37 +0000

    added

---
+++ nva.borrow/trunk/nva/borrow/browser/categories.py
@@ -0,0 +1,17 @@
+﻿from Products.Five.browser import BrowserView
+from Products.CMFCore.utils import getToolByName
+from .. import config
+
+class Categories(BrowserView):
+
+    def getCategories(self):
+        return config.CATEGORIES
+
+    def itemsWithCategory(self, category):
+        items = list()
+        catalog = getToolByName(self.context, 'portal_catalog')
+        for brain in catalog({'portal_type' : 'nva.borrow.borrowableitem'}):
+            item = brain.getObject()
+            if category in item.categories:
+                items.append(item)
+        return items
