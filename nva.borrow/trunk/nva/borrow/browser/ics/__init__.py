0545fb4177b02e10a6bb8b9f2a124a4e3f4db896
zopyxfilter <zopyxfilter@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2012-10-09 09:55:52 +0000

    added

---
+++ nva.borrow/trunk/nva/borrow/browser/ics/__init__.py
@@ -0,0 +1,20 @@
+﻿import os
+import glob
+import icalendar
+
+def getHolidays():
+
+    events = dict()
+    for ics in glob.glob('%s%s*.ics' % (os.path.dirname(__file__), os.path.sep)):
+        print ics
+        cal = icalendar.Calendar.from_ical(open(ics,'rb').read())
+        for item in cal.walk():
+            if isinstance(item, icalendar.cal.Event):
+                desc = item['DESCRIPTION']
+                start = item['DTSTART'].dt
+                end = item['DTEND'].dt
+                events[start] = desc
+    return events
+
+if __name__ == '__main__':
+    print getHolidays()
