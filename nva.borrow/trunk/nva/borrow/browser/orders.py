f7c6ad8aa4ed0c95565894cba3fea6f3ef97e99b
zopyxfilter@gmail.com <zopyxfilter@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2012-10-15 05:20:57 +0000

    savepoint

--- nva.borrow/trunk/nva/borrow/browser/orders.py
+++ nva.borrow/trunk/nva/borrow/browser/orders.py
@@ -1,12 +1,18 @@
 ﻿from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
-from datetime import datetime
+from datetime import datetime, timedelta
 import ics
 
 class Orders(BrowserView):
 
-    def getDateRange(self, num_days=30):
-        return ics.getDateRange(datetime.now().date(), num_days)
+    def getDateRange(self, start_date=None, start_offset_days=14, num_days=30):
+        """ Calculate the day range for the matrix based on a given 'start_date'
+            or <current date> + <start_offset_days> (2 weeks by default)
+
+        """
+        if not start_date:
+            start_date = datetime.now() + timedelta(days=start_offset_days)
+        return ics.getDateRange(start_date, num_days)
 
     def getItems(self):
         catalog = getToolByName(self.context, 'portal_catalog')
