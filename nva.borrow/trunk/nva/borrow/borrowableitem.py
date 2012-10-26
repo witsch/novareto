9aa0edc860a034c888916d4e7b6ad28923b58892
zopyxfilter@gmail.com <zopyxfilter@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2012-10-26 07:46:45 +0000

    updated

--- nva.borrow/trunk/nva/borrow/borrowableitem.py
+++ nva.borrow/trunk/nva/borrow/borrowableitem.py
@@ -1,35 +1,26 @@
 # -*- coding: iso-8859-1 -*-
 
 import time
+from datetime import date
+
 from five import grok
-from plone.directives import dexterity, form
-
 from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
 from zope.interface import invariant, Invalid
-
 from z3c.form import group, field
+from z3c.relationfield.schema import RelationList, RelationChoice
 
+from plone.app.textfield import RichText
+from plone.directives import dexterity, form
 from plone.namedfile.interfaces import IImageScaleTraversable
 from plone.namedfile.field import NamedImage, NamedFile
 from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
-from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
 from plone.formwidget.contenttree import ObjPathSourceBinder
 
+from Products.ATContentTypes.interfaces.image import IImageContent
+from Products.CMFCore.utils import getToolByName
+
 from nva.borrow import MessageFactory as _
-
-from five import grok
-from zope import schema
-
-from plone.directives import form, dexterity
-
-from plone.app.textfield import RichText
-from plone.namedfile.field import NamedImage
 import config
 
 # Interface class; used to define content-type schema.
@@ -42,9 +33,7 @@ categoryItems = SimpleVocabulary([SimpleVocabulary.createTerm(cat, cat)
 
 
 class IBorrowableItem(form.Schema, IImageScaleTraversable):
-    """
-    Borrowable item
-    """
+    """ Borrowable item """
 
     text = RichText(
             title=_(u'Text'),
@@ -73,6 +62,12 @@ class IBorrowableItem(form.Schema, IImageScaleTraversable):
             required=True,
         )
 
+    imageReference = RelationChoice(
+                title=_(u'Image reference (experimental)'),
+                description=_(u'Image reference (experimental)'),
+                source=ObjPathSourceBinder(object_provides=IImageContent.__identifier__),
+                required=False)
+
     categories = schema.List(
                     title=u'Categories',
                     value_type=schema.Choice(vocabulary=categoryItems),
@@ -85,7 +80,7 @@ class BorrowableItem(dexterity.Item):
     # Add your class methods and properties here
                                 
     def getBorrowRequests(self):
-        """ Return all borrow requests referencing the curret item"""
+        """ Return all borrow requests referencing the current item"""
         from Acquisition import aq_inner
         from zope.component import getUtility
         from zope.intid.interfaces import IIntIds
@@ -106,7 +101,6 @@ class BorrowableItem(dexterity.Item):
 
     def isBorrowable(self, start=None, end=None):
         """ Is current item borrowable from start-end ?"""
-        from datetime import date
         start = date(2012,8,10)
         end = date(2012,8,17)
         conflicting_requests = list()
@@ -118,62 +112,24 @@ class BorrowableItem(dexterity.Item):
 
     def getBookingDates(self):
         """ Return all booking dates for this particular item """
-        bookings = [dict(fromDate=request.buchungStart.strftime('%d.%m.%Y'), 
-                         toDate=request.buchungEnde.strftime('%d.%m.%Y'))
-                    for request in self.getBorrowRequests()]
+
+        intid = str(self.restrictedTraverse('@@getIntId')())
+        bookings = list()
+        catalog = getToolByName(self, 'portal_catalog')
+        for brain in catalog({'portal_type' : 'nva.borrow.borrowrequest'}):
+            br = brain.getObject()
+            for d in eval(br.formData)['items']:
+                if d['id'] == intid and br.buchungStart:
+                    bookings.append(dict(fromDate=br.buchungStart,
+                                         toDate=br.buchungEnde,
+                                         url=br.absolute_url(),
+                                         title=br.Title(),
+                                         numberItems=int(d['number'])))
+
         bookings.sort(lambda x,y: cmp(time.strptime(x['fromDate'], '%d.%m.%Y'),
                                       time.strptime(y['fromDate'], '%d.%m.%Y')))
         return bookings
 
-    def getBookingCalendar(self):
-        """ return a booking calendar data structure """
-
-        import calendar
-        from datetime import datetime, date, timedelta
-
-        booking_requests = self.getBorrowRequests()
-
-        result = list()
-        now = datetime.now().replace(day=15)
-        for num in range(0, 12):
-            next_first = (now + timedelta(days=30*num)).replace(day=1)
-            days_in_month = calendar.monthrange(next_first.year, next_first.month)[1]
-            d = dict(year=next_first.year, month=next_first.month, month_name=MONTH_NAMES[next_first.month-1], days=[])
-            for day in range(1, days_in_month+1):
-                current_day = date(next_first.year, next_first.month, day)
-
-
-                # default status
-                status = 'free'
-
-                # weekend
-                if current_day.weekday() in [5,6]:
-                    status = 'weekend'
-
-                # check against booking requests
-                relevant_requests = [r
-                                     for r in booking_requests
-                                     if r.buchungStart <= current_day and current_day <= r.buchungEnde]
-
-                if len(relevant_requests) >= self.itemsAvailable:
-                    status = 'blocked'
-
-                d['days'].append(dict(day=current_day.day, 
-                                      weekday=current_day.weekday(),
-                                      status=status))
-            result.append(d)
-
-        return result
-
-# View class
-# The view will automatically use a similarly named template in
-# borrowableitem_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
 
 class IndexView(grok.View):
     grok.context(IBorrowableItem)
