137716570462485b88def61f30f907043dbc131e
lwalthernovareto <lwalthernovareto@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2013-03-19 08:57:28 +0000

    helper-Funktionen fuer die Formatierung der Abschlusseite und des Drucks

---
+++ nva.bsbetreuung/trunk/nva/bsbetreuung/lib/helpers.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2004-2009 novareto GmbH
+# lwalther@novareto.de
+from Products.CMFCore.utils import getToolByName
+from uv.bsbetreuung import bsbetreuungMessageFactory as _
+
+
+def portal_catalog(context):
+    return getToolByName(context, 'portal_catalog')
+
+
+def formatFragen(context):
+    pcat = portal_catalog(context)
+    brains = pcat(portal_type = 'Fragestellung', review_state='published', show_inactive=True)
+    fragen = {}
+    for i in brains:
+        mydict = {}
+        frage = i.getObject()
+        mydict['title'] = frage.title
+        mydict['fieldtype'] = frage.fieldtype
+        optionen = {}
+        for j in frage.optionen:
+            option = j.split('|')
+            optionen[option[0]] = option[1]
+        mydict['optionen'] = optionen
+        fragen[frage.id] = mydict
+    return fragen
+
+
+def formatAufgaben(context):
+    pcat = portal_catalog(context)
+    brains = pcat(portal_type = 'Aufgabe', review_state='published', show_inactive=True)
+    aufgaben = {}
+    for i in brains:
+        aufgabe = i.getObject()
+        aufgaben[aufgabe.nummer] = aufgabe.title
+    return aufgaben
+
