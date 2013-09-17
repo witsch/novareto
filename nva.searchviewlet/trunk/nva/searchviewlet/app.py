bf48d38a41119168b50594b8114ba1f944cf6866
lwalthernovareto@gmail.com <lwalthernovareto@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2013-09-17 12:26:57 +0000

    Anpassung des Viewlets an das Design

--- nva.searchviewlet/trunk/nva/searchviewlet/app.py
+++ nva.searchviewlet/trunk/nva/searchviewlet/app.py
@@ -1,3 +1,4 @@
+import string
 from zope.interface import Interface
 from uvc.api import api
 from uvc.api.api import grok
@@ -22,21 +23,31 @@ class NvaSearch(api.Page):
     def portal_url(self):
         return getToolByName(self.context, 'portal_url').getPortalObject().absolute_url() 
 
-    def checkWebcode(self, suchtext):
+    def checkWebcode(self, suchtext, path):
         pcat = self.portal_catalog
-        brains = self.portal_catalog(Webcode=suchtext)
+        if path:
+            brains = self.portal_catalog(Webcode=suchtext, path=path)
+        else:
+            brains = self.portal_catalog(Webcode=suchtext)
         url = ''
         if len(brains) == 1:
             url = brains[0].getURL()
-        elif len(brains) > 1:
+        elif len(brains) > 1 and path:
+            url = '%s/@@search?Webcode=%s&path=%s' %(self.portal_url, suchtext, path)
+        elif len(brains) > 1 and not path:
             url = '%s/@@search?Webcode=%s' %(self.portal_url, suchtext)
         return url
 
     def render(self):
         suchtext =  self.request.get("SearchableText", "")
+        path =  self.request.get("path", "")
+        if path:
+           string.join(path[:-1], '/')
         if suchtext:
-            webcodeurl = self.checkWebcode(suchtext)
+            webcodeurl = self.checkWebcode(suchtext, path)
             if webcodeurl:
                 return self.request.response.redirect(webcodeurl)
         url = '%s/@@search?SearchableText=%s' %(self.portal_url, suchtext)
+        if path:
+            url = '%s/@@search?SearchableText=%s&path=%s' %(self.portal_url, suchtext, path)
         return self.request.response.redirect(url)
