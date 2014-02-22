bb82b15d2a4fbacbd2c9655183b42a4cd8ebf1dc
lwalthernovareto@gmail.com <lwalthernovareto@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2014-02-22 08:00:57 +0000

    In der Navigation wird ein eigenes Template verwendet

--- nva.customnavigation/trunk/nva/customnavigation/browser/navigation.py
+++ nva.customnavigation/trunk/nva/customnavigation/browser/navigation.py
@@ -1,91 +1,12 @@
 # -*- coding: utf-8 -*-
 from plone.app.portlets.portlets.navigation import Renderer
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.CMFCore.utils import getToolByName
-from Products.CMFCore.interfaces import IFolderish
-from Acquisition import aq_inner, aq_base, aq_parent
 
 class MyNavRenderer(Renderer):
 
-    @property
-    def isportalcontext(self):
-        """ Diese Funktion liefert einen Boolwert zurück der aussagt,
-            ob wir im direkten Context des Portals stehen. In diesem
-            Fall soll der Backlink nicht angeboten werden. """
-        context = self.context.aq_inner
-        portal = getToolByName(self.context, 'portal_url').getPortalObject()
-        if context == portal:
-            return True
-        if not IFolderish.providedBy(context):
-            if context.aq_parent == portal:
-                return True
-        return False
-
-    @property
-    def isdefaultpage(self):
-        """ Diese Funktion liefert einen Boolwert zurück der aussagt,
-            ob es sich beim Context um die Standardansicht eines Ordners
-            handelt. """
-        context = self.context.aq_inner
-        if not IFolderish.providedBy(context) or context.portal_type == 'Topic':
-            if hasattr(context.aq_parent, 'default_page'):
-                if context.id == context.aq_parent.default_page:
-                    return True
-        return False
-
-    @property
-    def available(self):
-        """ Realisiert die Verfuegbarkeit des Backlinks fuer den Fall,
-            dass nur ein Ordner mit Startseite angelegt wurde. """
-        rootpath = self.getNavRootPath()
-        if rootpath is None:
-            return False
-
-        if self.data.bottomLevel < 0:
-            return True
-
-        tree = self.getNavTree()
-        retval =  len(tree['children']) > 0
-        if not self.isportalcontext:
-            retval = True
-        return retval
-
     def render(self):
-        """ Ueberschreibt den Standard-Renderer fuer das Navigations-
-            portlet. Im Kopf des Navigationsportlets soll ein Zurueck-
-            link angezeigt werden damit vor allem im mobilen Bereich
-            eine Off-Canvas zurueck Navigation erreicht werden kann. """
+        """ Es soll lediglich das eigene Template genutzt werden. """
 
-        back = self.context.aq_inner.aq_parent
-        context = self.context.aq_inner
-
-        self.heading_link_target = ""
-
-        #In der Wurzel des Plone-Portals soll kein Backlink erscheinen
-        if self.isportalcontext:
-            self.back_name = ''
-            self.back_url = ''
-            return self._template()
-
-        self.back_name = back.title
-        self.back_url = back.absolute_url()
-        #Sonderfall-1: Folderview in der 1. Ebene, zurueck zur Startseite
-        if IFolderish.providedBy(context):
-            if back.portal_type == 'Plone Site':
-                self.back_name = u'Startseite'
-                return self._template()
-
-        #Sonderfall-2: Pruefung, ob es sich beim Contextobjekt um eine Standard-
-        #              ansicht eines Ordners handelt.
-        if self.isdefaultpage:
-            #Variante-2a: Contextobjekt ist Standardansicht bei einem Ordner der 1. Ebene.
-            if back.aq_parent.portal_type == 'Plone Site':
-                self.back_name = 'Startseite'
-                self.back_url = back.aq_parent.absolute_url()
-            #Variante-2b: Contextobjekt ist Standardansicht eines Ordners beliebiger Ebene.
-            else:
-                self.back_name = back.aq_parent.title
-                self.back_url = back.aq_parent.absolute_url()
         return self._template()
 
     _template = ViewPageTemplateFile('navigation.pt')
