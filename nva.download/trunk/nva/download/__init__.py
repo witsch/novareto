210a6aa00ad3881f66179fbb7500389d0f0b75bb
lwalthernovareto@gmail.com <lwalthernovareto@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2014-02-26 14:37:20 +0000

    Umstellung auf uvc.api

--- nva.download/trunk/nva/download/__init__.py
+++
@@ -1,7 +0,0 @@
-  # -*- extra stuff goes here -*- 
-from zope.i18nmessageid import MessageFactory
-
-downloadMessageFactory = MessageFactory('nva.download')
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
