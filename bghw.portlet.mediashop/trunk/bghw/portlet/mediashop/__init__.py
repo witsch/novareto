62fb26bcfc1a1450a2e21377ff311e1737dacd60
lwalthernovareto@gmail.com <lwalthernovareto@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2014-04-15 13:55:25 +0000

    Initialimport

---
+++ bghw.portlet.mediashop/trunk/bghw/portlet/mediashop/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+MediashopPortletMessageFactory = MessageFactory('bghw.portlet.mediashop')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
---
+++ bghw.portlet.mediashop/trunk/bghw/portlet/mediashop/mediashopportlet.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+from zope import schema
+from zope.interface import implements
+from plone.app.vocabularies.catalog import SearchableTextSourceBinder
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.app.portlets.portlets import base
+from bghw.mediashop.tocard import getSessionCookie
+
+# TODO: If you define any fields for the portlet configuration schema below
+# do not forget to uncomment the following import
+#from zope import schema
+from zope.formlib import form
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+# TODO: If you require i18n translation for any of your schema fields below,
+# uncomment the following to import your package MessageFactory
+#from bghw.portlet.mediashop import MediashopPortletMessageFactory as _
+
+
+class IMediashopPortlet(IPortletDataProvider):
+    """A portlet
+
+    It inherits from IPortletDataProvider because for this portlet, the
+    data that is being rendered and the portlet assignment itself are the
+    same.
+    """
+
+    # TODO: Add any zope.schema fields here to capture portlet configuration
+    # information. Alternatively, if there are no settings, leave this as an
+    # empty interface - see also notes around the add form and edit form
+    # below.
+
+    # some_field = schema.TextLine(title=_(u"Some field"),
+    #                              description=_(u"A field to use"),
+    #                              required=True)
+
+    titel = schema.TextLine(title = u"Titel des Portlets",
+                            required = True)
+
+
+    shop = schema.Choice(title= u"Auswahl des Medienshops",
+                         description=u"Bitte waehlen Sie hier den Ordner aus in dem sich der Mediashop befindet.",
+                         required=True,
+                         source=SearchableTextSourceBinder({'is_folderish': True},
+                                                            default_query='path:'))
+
+
+class Assignment(base.Assignment):
+    """Portlet assignment.
+
+    This is what is actually managed through the portlets UI and associated
+    with columns.
+    """
+
+    implements(IMediashopPortlet)
+
+    # TODO: Set default values for the configurable parameters here
+
+    # some_field = u""
+
+    titel = u"Mein Warenkorb"
+    shop = None
+
+    # TODO: Add keyword parameters for configurable parameters here
+    # def __init__(self, some_field=u""):
+    #    self.some_field = some_field
+
+    def __init__(self, titel=u"Mein Warenkorb", shop=None):
+        self.titel = titel
+        self.shop = shop
+
+    @property
+    def title(self):
+        """This property is used to give the title of the portlet in the
+        "manage portlets" screen.
+        """
+        return "Medienshop Portlet"
+
+
+class Renderer(base.Renderer):
+    """Portlet renderer.
+
+    This is registered in configure.zcml. The referenced page template is
+    rendered, and the implicit variable 'view' will refer to an instance
+    of this class. Other methods can be added and referenced in the template.
+    """
+
+    render = ViewPageTemplateFile('mediashopportlet.pt')
+
+    @property
+    def available(self):
+        return self.context.aq_inner.session_data_manager.hasSessionData()
+
+    @property
+    def artikel(self):
+        return len(getSessionCookie(self.context.aq_inner).keys())
+
+    @property
+    def shop_url(self):
+        return self.context.aq_inner.portal_url() + self.data.shop + '/medienbestellung'
+
+class AddForm(base.AddForm):
+    """Portlet add form.
+
+    This is registered in configure.zcml. The form_fields variable tells
+    zope.formlib which fields to display. The create() method actually
+    constructs the assignment that is being added.
+    """
+    form_fields = form.Fields(IMediashopPortlet)
+
+    def create(self, data):
+        return Assignment(**data)
+
+
+# NOTE: If this portlet does not have any configurable parameters, you
+# can use the next AddForm implementation instead of the previous.
+
+# class AddForm(base.NullAddForm):
+#     """Portlet add form.
+#     """
+#     def create(self):
+#         return Assignment()
+
+
+# NOTE: If this portlet does not have any configurable parameters, you
+# can remove the EditForm class definition and delete the editview
+# attribute from the <plone:portlet /> registration in configure.zcml
+
+
+class EditForm(base.EditForm):
+    """Portlet edit form.
+
+    This is registered with configure.zcml. The form_fields variable tells
+    zope.formlib which fields to display.
+    """
+    form_fields = form.Fields(IMediashopPortlet)
