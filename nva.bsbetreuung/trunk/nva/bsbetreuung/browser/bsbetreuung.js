2232a86f2f9eee0b5adfb25888a11796ae57a2dd
lwalthernovareto@gmail.com <lwalthernovareto@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2014-06-28 21:11:44 +0000

    Umstellung javascript

--- nva.bsbetreuung/trunk/nva/bsbetreuung/browser/bsbetreuung.js
+++ nva.bsbetreuung/trunk/nva/bsbetreuung/browser/bsbetreuung.js
@@ -1,58 +1,58 @@
-jq(document).bind('ready', function() {
+$(document).bind('ready', function() {
 
-    jq('span.required').empty();
+    /* $('span.required').empty(); */
 
-    if (jq('dl.portlet.portletErgebnisPortlet').length !=0) {
-        var fixedwidth = jq('dl.portlet.portletDGUV2Help').width();
+    if ($('dl.portlet.portletErgebnisPortlet').length !=0) {
+        var fixedwidth = $('dl.portlet.portletDGUV2Help').width();
     }
-    if (jq('dl.portlet.portletDGUV2Help').length != 0) {
-        var xportlet = jq('dl.portlet.portletDGUV2Help').position().left;
-        jq('dl.portlet.portletDGUV2Help').css({width:fixedwidth});
+    if ($('dl.portlet.portletDGUV2Help').length != 0) {
+        var xportlet = $('dl.portlet.portletDGUV2Help').position().left;
+        $('dl.portlet.portletDGUV2Help').css({width:fixedwidth});
     }
-    if (jq('fieldset#hh_fragen').length != 0) {
-        var yfieldset = jq('fieldset#hh_fragen').position().top;
+    if ($('fieldset#hh_fragen').length != 0) {
+        var yfieldset = $('fieldset#hh_fragen').position().top;
     }
-    if (jq('input[value=ja]').length != 0) {
-        var yyes = jq('input[value=ja]').position().top;
+    if ($('input[value=ja]').length != 0) {
+        var yyes = $('input[value=ja]').position().top;
     }
 
-    jq('fieldset#hh_fragen').hide();
-    jq('div#nofields').hide();
+    $('fieldset#hh_fragen').hide();
+    $('div#nofields').hide();
 
-    var boolchecked = jq("input#form\\.mybool\\.0").attr('checked');
+    var boolchecked = $("input#form\\.mybool\\.0").attr('checked');
 
     if (boolchecked == true) {
-            jq('fieldset#hh_fragen').show();
-            jq('dl.portlet.portletDGUV2Help').css({width:fixedwidth, position:"absolute", top:yfieldset, left:xportlet});
+            $('fieldset#hh_fragen').show();
+            $('dl.portlet.portletDGUV2Help').css({width:fixedwidth, position:"absolute", top:yfieldset, left:xportlet});
     }
     
-    jq('input[value=nein]').bind('click', function() {
-        jq('fieldset#hh_fragen').fadeOut();
-        jq('div#nofields').fadeIn();
-        jq('dl.portlet.portletDGUV2Help').css({width:fixedwidth, position:"absolute", top:yyes, left:xportlet});
+    $('input[value=nein]').bind('click', function() {
+        $('fieldset#hh_fragen').fadeOut();
+        $('div#nofields').fadeIn();
+        $('dl.portlet.portletDGUV2Help').css({width:fixedwidth, position:"absolute", top:yyes, left:xportlet});
 
      });
 
-    jq('input[value=ja]').bind('click', function() {
-        jq('div#nofields').fadeOut();
-        jq('fieldset#hh_fragen').fadeIn();
-        jq('dl.portlet.portletDGUV2Help').css({width:fixedwidth, position:"absolute", top:yfieldset, left:xportlet});
+    $('input[value=ja]').bind('click', function() {
+        $('div#nofields').fadeOut();
+        $('fieldset#hh_fragen').fadeIn();
+        $('dl.portlet.portletDGUV2Help').css({width:fixedwidth, position:"absolute", top:yfieldset, left:xportlet});
      });
 
-    var helplinks = jq('a.hhportlet_help');
-    jq(helplinks).click(function(event, data) {
+    var helplinks = $('a.hhportlet_help');
+    $(helplinks).click(function(event, data) {
         var myurl = event.currentTarget + "/helpwindow_view";
         window.open(myurl, 'Continue_to_Application', 'width=600, height=500, resizable=1');
         event.preventDefault();
     });
 
-    var bshelplinks = jq('a.bs_help');
-    jq(bshelplinks).click(function(event, data) {
+    var bshelplinks = $('a.bs_help');
+    $(bshelplinks).click(function(event, data) {
         var myurl = event.currentTarget;
         window.open(myurl, 'Continue_to_Application', 'width=600, height=500, resizable=1');
         event.preventDefault();
     });
 
-    jq.ajaxSetup({cache: false});
+    $.ajaxSetup({cache: false});
 
 })    
