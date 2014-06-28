f53a8090cd1ecd77d229865a42a7498d73004f60
lwalthernovareto@gmail.com <lwalthernovareto@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2014-06-28 21:32:42 +0000

    Umstellung javascript

--- bghw.seminare/trunk/bghw/seminare/browser/customBghwSeminare.js
+++ bghw.seminare/trunk/bghw/seminare/browser/customBghwSeminare.js
@@ -1,30 +1,30 @@
-jq (document).ready(function() {
-    jq('fieldset#pfg-fieldsetname-angaben-zur-ansprechperson').hide();
-    jq('div#archetypes-fieldname-beschreibung-der-funktion-im-betrieb').hide();
+$(document).ready(function() {
+    $('fieldset#pfg-fieldsetname-angaben-zur-ansprechperson').hide();
+    $('div#archetypes-fieldname-beschreibung-der-funktion-im-betrieb').hide();
 
-    var boolchecked = jq('input#uebereinstimmung_2').attr('checked');
+    var boolchecked = $('input#uebereinstimmung_2').attr('checked');
 
     if (boolchecked == true) {
-        jq('fieldset#pfg-fieldsetname-angaben-zur-ansprechperson').show();
+        $('fieldset#pfg-fieldsetname-angaben-zur-ansprechperson').show();
     }
 
-    jq('input#uebereinstimmung_2').click(function() {
-        jq('fieldset#pfg-fieldsetname-angaben-zur-ansprechperson').fadeIn();
+    $('input#uebereinstimmung_2').click(function() {
+        $('fieldset#pfg-fieldsetname-angaben-zur-ansprechperson').fadeIn();
     })
-    jq('input#uebereinstimmung_1').click(function() {
-        jq('fieldset#pfg-fieldsetname-angaben-zur-ansprechperson').fadeOut();
+    $('input#uebereinstimmung_1').click(function() {
+        $('fieldset#pfg-fieldsetname-angaben-zur-ansprechperson').fadeOut();
     })
-    jq('input#berufliche-voraussetzungen_4').click(function() {
-        jq('div#archetypes-fieldname-beschreibung-der-funktion-im-betrieb').fadeIn();
+    $('input#berufliche-voraussetzungen_4').click(function() {
+        $('div#archetypes-fieldname-beschreibung-der-funktion-im-betrieb').fadeIn();
     })
-    jq('input#berufliche-voraussetzungen_3').click(function() {
-        jq('div#archetypes-fieldname-beschreibung-der-funktion-im-betrieb').fadeOut();
+    $('input#berufliche-voraussetzungen_3').click(function() {
+        $('div#archetypes-fieldname-beschreibung-der-funktion-im-betrieb').fadeOut();
     })
-    jq('input#berufliche-voraussetzungen_2').click(function() {
-        jq('div#archetypes-fieldname-beschreibung-der-funktion-im-betrieb').fadeOut();
+    $('input#berufliche-voraussetzungen_2').click(function() {
+        $('div#archetypes-fieldname-beschreibung-der-funktion-im-betrieb').fadeOut();
     })
-    jq('input#berufliche-voraussetzungen_1').click(function() {
-        jq('div#archetypes-fieldname-beschreibung-der-funktion-im-betrieb').fadeOut();
+    $('input#berufliche-voraussetzungen_1').click(function() {
+        $('div#archetypes-fieldname-beschreibung-der-funktion-im-betrieb').fadeOut();
     })
 })
 
