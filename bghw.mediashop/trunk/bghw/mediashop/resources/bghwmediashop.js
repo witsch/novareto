ac24b736ac75b9fe2a7483a9b9308f15fca3ac54
lwalthernovareto@gmail.com <lwalthernovareto@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2014-06-28 20:59:56 +0000

    Umstellung javascript

--- bghw.mediashop/trunk/bghw/mediashop/resources/bghwmediashop.js
+++ bghw.mediashop/trunk/bghw/mediashop/resources/bghwmediashop.js
@@ -1,18 +1,17 @@
-jq(document).ready(function() {
-    jq('.field-collection-move-up').hide();
-    jq('.field-collection-move-down').hide();
-    jq('input[name="form.field.bestellung.add"]').hide();
-    jq("input[id$=field-artikel]").prop('readonly', true);
-    jq("input[id$=field-artikel]").css('width', '4em');
-    jq("input[id$=field-bestellnummer]").prop('readonly', true);
-    jq("input[id$=field-bestellnummer]").css('width', '7em');
-    jq("input[id$=field-beschreibung]").prop('readonly', true);
-    jq("input[id$=field-anzahl]").css('width', '4em');
+$(document).ready(function() {
+    $('.field-collection-move-up').hide();
+    $('.field-collection-move-down').hide();
+    $('input[name="form.field.bestellung.add"]').hide();
+    $("input[id$=field-artikel]").prop('readonly', true);
+    $("input[id$=field-artikel]").css('width', '4em');
+    $("input[id$=field-bestellnummer]").prop('readonly', true);
+    $("input[id$=field-bestellnummer]").css('width', '7em');
+    $("input[id$=field-beschreibung]").prop('readonly', true);
+    $("input[id$=field-anzahl]").css('width', '4em');
 
-    jq('fieldset#bghw\\.medienshop\\.lieferung').hide()
+    $('fieldset#bghw\\.medienshop\\.lieferung').hide()
 
-    jq("#form-field-lieferung").click(function() {
-        jq('fieldset#bghw\\.medienshop\\.lieferung').toggle();
+    $("#form-field-lieferung").click(function() {
+        $('fieldset#bghw\\.medienshop\\.lieferung').toggle();
     });
-
 });
