f94c209002802acb4c88ab04b29d684bb6701045
ck@novareto.de <ck@novareto.de@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2014-09-10 12:38:13 +0000

    change to the other skin

--- chemiedp_project/trunk/src/chemiedp/src/chemiedp/utils.py
+++ chemiedp_project/trunk/src/chemiedp/src/chemiedp/utils.py
@@ -4,19 +4,16 @@ import uvclight
 from cromlech.configuration.utils import load_zcml
 from cromlech.i18n import register_allowed_languages
 from cromlech.dawnlight import DawnlightPublisher
-from cromlech.browser import PublicationBeginsEvent, PublicationEndsEvent
 from cromlech.browser import IPublicationRoot
 from zope.interface import implements
-from zope.location import Location
-from zope.component.hooks import setSite
-from zope.event import notify
 from cromlech.webob.request import Request
 from cromlech.dawnlight import ViewLookup
 from cromlech.dawnlight import view_locator, query_view
-from zope.component import getGlobalSiteManager
 from uvclight.backends.zodb import Root
 from cromlech.zodb import get_site, Site
 from cromlech.zodb import initialize_applications
+from zope.interface import alsoProvides
+from uvc.themes.dguv import IDGUVRequest
 
 view_lookup = ViewLookup(view_locator(query_view))
 
@@ -34,7 +31,13 @@ publisher = DawnlightPublisher(view_lookup=view_lookup)
 
 class Application(object):
 
-    def __init__(self, global_conf, name, zcml_file=None, session_key=None, langs='en'):
+    def __init__(self,
+                 global_conf,
+                 name,
+                 zcml_file=None,
+                 session_key=None,
+                 langs='en'
+                 ):
         load_zcml(zcml_file)
         allowed = langs.strip().replace(',', ' ').split()
         register_allowed_languages(allowed)
@@ -42,10 +45,11 @@ class Application(object):
         self.session_key = session_key
 
     def __call__(self, environ, start_response):
-        
+
         @uvclight.sessionned(self.session_key)
         def publish(environ, start_response):
             request = Request(environ)
+            alsoProvides(request, IDGUVRequest)
             conn = request.environment[KEY]
             site = get_site(conn, self.name)
             with Site(site) as site:
@@ -54,5 +58,6 @@ class Application(object):
 
         return publish(environ, start_response)
 
+
 def create_db(db):
-    initialize_applications(db, lambda: {'chemiedp' : Root})
+    initialize_applications(db, lambda: {'chemiedp': Root})
--- chemiedp_project/trunk/src/chemiedp/src/chemiedp/views.py
+++ chemiedp_project/trunk/src/chemiedp/src/chemiedp/views.py
@@ -1,10 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2007-2013 NovaReto GmbH
+# cklinger@novareto.de
+
 import uvclight
 from uvc.api import api
+from uvclight import MenuItem
+from uvc.design.canvas import IGlobalMenu, IPersonalMenu
+
 
 class Index(api.Page):
     api.context(uvclight.IRootObject)
 
     def render(self):
-        import pdb;pdb.set_trace()
         return "Hallo Welt"
 
+
+class Startseite(MenuItem):
+    api.name('index')
+    api.title('Startseite')
+    api.menu(IGlobalMenu)
+    api.context(uvclight.IRootObject)
+
+
+class Benutzername(MenuItem):
+    api.name('benutzername')
+    api.title('Benutzername')
+    api.menu(IPersonalMenu)
+    api.context(uvclight.IRootObject)
