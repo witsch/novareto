44e4e1caf0419e7cc58c8ca9782cbfaa96dc99f2
ck@novareto.de <ck@novareto.de@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2014-09-23 09:42:34 +0000

    changes zodb

--- chemiedp_project/src/chemiedp/src/chemiedp/utils.py
+++ chemiedp_project/src/chemiedp/src/chemiedp/utils.py
@@ -8,9 +8,17 @@ from zope.component import getGlobalSiteManager
 from uvclight.utils import Site
 from uvclight.bricks import Publication  # SecurePublication
 from uvclight.backends.zodb import Root, ZODBPublication
+from uvclight.utils import with_zcml, with_i18n
+
+
+class MyRoot(Root):
+    pass
 
 
 class Application(ZODBPublication):
 
-    def site_manager(self, environ):
-        return Site(Root, self.name)
+    @classmethod
+    @with_zcml('zcml_file')
+    @with_i18n('langs', fallback='en')
+    def create(cls, gc, **kwargs):
+        return super(Application, cls).create(root=MyRoot, **kwargs)
--- chemiedp_project/src/chemiedp/src/chemiedp/views.py
+++ chemiedp_project/src/chemiedp/src/chemiedp/views.py
@@ -1,7 +1,9 @@
 from zope.interface import Interface
 from uvc.api import api
 
-class Index(api.Page):
+
+class Index(api.View):
     api.context(Interface)
+
     def render(self):
         return "Das ist meine Startseite"
