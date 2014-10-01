91a27ff3c04a356d738854fc292e248d648e6823
lwalthernovareto@gmail.com <lwalthernovareto@gmail.com@deb551aa-4b7b-11de-9241-1f6e7bca54ae>
2014-10-01 09:26:40 +0000

    Produktionsbetrieb

--- nva.iframe/trunk/nva/iframe/resources/change_url.js
+++ nva.iframe/trunk/nva/iframe/resources/change_url.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('a').each(function(){
-        this.href = this.href.replace('http://vedaentwicklung-etem.bg-kooperation.de', 'http://entwicklung-etem.bg-kooperation.de/seminare/vedaseminare');
+        this.href = this.href.replace('http://seminardatenbank.bgetem.de', 'http://www.bgetem.de/seminare/seminardatenbank-1/seminardatenbank');
         this.href = this.href.replace(';', '&DetailID=');
     });
 });
